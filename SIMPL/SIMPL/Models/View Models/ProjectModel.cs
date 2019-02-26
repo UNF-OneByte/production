2793b646667981abf7246ed0b7740bbbbae32543
myishakinberg <mskinberg@gmail.com>
2019-02-26 15:34:17 -0500

    Datatables progress

---
+++ SIMPL/SIMPL/Models/View Models/ProjectModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SIMPL.Models.View_Models
+{
+    public class ProjectModel
+    {
+        string projectName { get; set; }
+        string projectManager { get; set; }
+        string projectStartDate { get; set; }
+        string projectDescription { get; set; }
+        string projectEstEndDate { get; set; }
+        string projectEstCost { get; set; }
+        string projectActCost { get; set; }
+        List<TaskModel> projectTaskList = new List<TaskModel>();
+    }
+}
--- SIMPL/SIMPL/wwwroot/js/site.js
+++ SIMPL/SIMPL/wwwroot/js/site.js
@@ -1,1 +1,10 @@
 ﻿// Write your JavaScript code.
+$(document).ready(function () {
+    $('#example').DataTable();
+});
+$(document).ready(function () {
+    var ho = document.getElementsByClassName(".sorting_1");
+    ho.hover(function () {
+        $(this).css("background-color", "yellow");
+    });
+});
