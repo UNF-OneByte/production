7d90e55585d67cc93deccf343c01d6b3330aa973
Shaun Young <1ShaunYoung1@gmail.com>
2019-02-28 09:50:31 -0500

    Tasks

--- SIMPL/SIMPL/Models/View Models/TaskModel.cs
+++ SIMPL/SIMPL/Models/View Models/TaskModel.cs
@@ -7,6 +7,18 @@ namespace SIMPL.Models.View_Models
 {
     public class TaskModel
     {
-
+        string Name { get; set; }
+        string Description { get; set; }
+        string CreatedById { get; set; }
+        int TaskId { get; set; }
+        int ProjectId { get; set; }
+        int VendorId { get; set; }
+        int LocationId { get; set; }
+        int CostTypleId { get; set; }
+        DateTime DateCreated { get; set; }
+        decimal EstimatedCost { get; set; }
+        decimal ActualCost { get; set; }
+        decimal EstimatedHours{ get; set; }
+        decimal ActualHours { get; set; }
     }
 }
