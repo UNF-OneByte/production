cbc37ed6004ad03274d11a7e6de242ac98d04699
Ryan Geddings <n01067534@ospreys.unf.edu>
2019-03-05 15:12:31 -0500

    Updated packages and Framework to .net 2.1

--- SIMPL/SIMPL/wwwroot/js/site.js
+++ SIMPL/SIMPL/wwwroot/js/site.js
@@ -5,13 +5,14 @@
 $('#projectInformation').on('shown.bs.modal', function () {
     alert("hello");
 
-﻿// Write your JavaScript code.
-$(document).ready(function () {
-    $('#example').DataTable();
-});
-$(document).ready(function () {
-    var ho = document.getElementsByClassName(".sorting_1");
-    ho.hover(function () {
-        $(this).css("background-color", "yellow");
+    // Write your JavaScript code.
+    $(document).ready(function () {
+        $('#example').DataTable();
     });
-});
+    $(document).ready(function () {
+        var ho = document.getElementsByClassName(".sorting_1");
+        ho.hover(function () {
+            $(this).css("background-color", "yellow");
+        });
+    })
+})
